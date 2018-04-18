150ce1be8ddeba3d77840e5ebeeb50bae1b4f557
Shaylee Chong <shayleechong@gmail.com>
2018-04-18 16:01:36 +0800

    ASK MEE ANYTHINGG

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomBallAnswer : Int = 0
+    
+    var ballImageNumber = ["ball1", "ball2", "ball3", "ball4", "ball5"]
 
+    @IBOutlet weak var imageBallAnswer: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        changeBallAnswer()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonAskMePressed(_ sender: Any) {
+        
+        changeBallAnswer()
+        
+    }
+    
+    func changeBallAnswer() {
+        
+        randomBallAnswer = Int(arc4random_uniform(5))
+        
+        imageBallAnswer.image = UIImage(named: ballImageNumber[randomBallAnswer] )
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        changeBallAnswer()
+        
+    }
+    
 }
 
