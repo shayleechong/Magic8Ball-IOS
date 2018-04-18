1308e84a1f34b19602d68b550dc9f6565dc191f8
Shaylee Chong <shayleechong@gmail.com>
2018-04-18 15:35:05 +0800

    Project Start

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Shaylee Chong on 18/4/18.
+//  Copyright © 2018 Shaylee Chong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
